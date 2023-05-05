706394ea3ae2210029d777b6a88fd43dde16fc00
twilkhoo <tejaswilkhoo@hotmail.com>
2023-05-04 20:46:10 -0400

    Add text and fading in transitions, with new navbar.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,166 @@
-import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import useAuth from "../hooks/useAuth";
+import { useRouter } from "next/router";
+import IndexBackground from "@/components/index/background/IndexBackground";
+import Navbar from "@/components/navbar/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { motion } from "framer-motion";
+import FadingText from "@/components/index/background/FadingText";
 
 const Home = () => {
-  const { user, login } = useAuth();
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [firstTextToggleParent, setFirstTextToggleParent] = useState(true);
+  const [firstTextToggle, setFirstTextToggle] = useState(true);
+
+  const { login } = useAuth();
 
   const tempHandleGoogleSignIn = async () => {
     await login();
+  };
+
+  const MotionText = motion(Text);
+
+  useEffect(() => {
+    axios.get("https://type.fit/api/quotes").then((res) => {
+      const data = res.data;
+      const date = new Date();
+      const index =
+        ((date.getDate() + date.getMonth() + date.getFullYear()) * 13) %
+        data.length;
+
+      setQuote(data[index].text);
+      setAuthor(data[index].author);
+    });
+  }, []);
+
+  const handleTextToggle = async () => {
+    setFirstTextToggle(!firstTextToggle);
+    setTimeout(() => {setFirstTextToggleParent(!firstTextToggleParent)}, 1000)
   }
 
+  const variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Box>
-        <Heading>Hello World</Heading>
-        <Text>User: {user?.displayName}</Text>
-        <Button onClick={tempHandleGoogleSignIn}>Sign in with google</Button>
-      </Box>
-    </Flex>
+    <IndexBackground>
+      <Navbar />
+
+      {firstTextToggleParent && 
+        <motion.div
+          variants={variants}
+          initial="visible"
+          animate={firstTextToggle ? "visible" : "hidden"}
+          transition={{ duration: 1 }}
+        >
+          <Box mx={[25, null, 50, 75, 100, 150]} mt="100px">
+            <FadingText delay={2}>
+              {" "}
+              <Text textStyle="homeText1">welcome, or welcome back,</Text>{" "}
+            </FadingText>
+            <FadingText delay={4}>
+              {" "}
+              <Text textStyle="homeText1">
+                it's nice to hear from you again.
+              </Text>{" "}
+            </FadingText>
+            <FadingText delay={6}>
+              <Text textStyle="homeText1" pt="50px">
+                your quote for today is-
+              </Text>
+            </FadingText>
+            <FadingText delay={8}>
+              <Text textStyle="homeText1" fontStyle="italic" pl="50px">
+                ; {quote}
+              </Text>
+            </FadingText>
+            <FadingText delay={9}>
+              <Text textStyle="homeText2" pl="50px">
+                ; {author}
+              </Text>
+            </FadingText>
+
+            <FadingText delay={11}>
+              <Text textStyle="homeText1" pt="50px">
+                {" "}
+                ready?{" "}
+              </Text>
+            </FadingText>
+
+            <FadingText delay={12}>
+              <Button
+                variant="outlined"
+                onClick={tempHandleGoogleSignIn}
+                px="80px"
+                py="30px"
+              >
+                <Text textStyle="buttonText1">enter</Text>
+              </Button>
+            </FadingText>
+
+            <FadingText delay={14}>
+              <Text
+                onClick={handleTextToggle}
+                textStyle="homeText2"
+                textDecoration="underline"
+                pt="25px"
+                _hover={{ cursor: "pointer" }}
+              >
+                (what is chromatic;notes?)
+              </Text>
+            </FadingText>
+          </Box>
+        </motion.div>
+      }
+
+      {
+        
+      }
+
+      {/* 
+      <Box mx={[25, null, 50, 75, 100, 150]} mt="200px">
+        <Text textStyle="homeText1" id="secondPart">
+          chromatic;notes is a{" "}
+          <Box as="span" textDecoration="line-through">
+            journal
+          </Box>{" "}
+          emotion tracker.
+        </Text>
+        <Text textStyle="homeText1">
+          once a day, you pick a color representing how you feel.
+        </Text>
+        <Text textStyle="homeText1">you can add a note if needed.</Text>
+        <Text textStyle="homeText1">
+          then, you can view how you've been feeling lately.
+        </Text>
+        <Text textStyle="homeText1" pt="50px">care to give it a try?</Text>
+        <Button
+          variant="outlined"
+          onClick={tempHandleGoogleSignIn}
+          px="80px"
+          py="30px"
+          mb="200px"
+        >
+          <Text textStyle="buttonText1">join with google</Text>
+        </Button>
+      </Box> */}
+    </IndexBackground>
   );
-}
+};
 
 export default Home;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,20 +1,103 @@
 import { extendTheme } from "@chakra-ui/react";
+import { Alex_Brush, Dancing_Script, Fredericka_the_Great, Inter, Merienda, Poiret_One, Sen, Stalemate } from "next/font/google"
+
+const poiretOne = Poiret_One({ subsets: ["latin"], weight: "400" });
+const inter = Inter({ subsets: ["latin"], weight: "400" });
+const fred = Fredericka_the_Great({ subsets: ["latin"], weight: "400" });
+const stalemate = Stalemate({ subsets: ["latin"], weight: "400" });
+const dancingScript = Dancing_Script({ subsets: ["latin"], weight: "400" });
+const alexBrush = Alex_Brush({ subsets: ["latin"], weight: "400" });
+const merienda = Merienda({ subsets: ["latin"], weight: "400" });
+const sen = Sen({ subsets: ["latin"], weight: "400" });
 
 const customTheme = extendTheme({
   styles: {
     global: () => ({
       body: {
-        bg: "background.grey.100",
+        bg: "white",
+      },
+      "::selection": {
+        background: "white",
+        color: "background.50"
+      },
+      "&::-webkit-scrollbar": {
+        width: "8px",
+      },
+      "&::-webkit-scrollbar-track": {
+        width: "8px",
+        backgroundColor: "transparent",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "white",
+        borderRadius: "8px",
       },
     }),
   },
   breakpoints: {
-    sm: "320px",
+    sm: "480px",
     md: "768px",
-    lg: "960px",
+    lg: "992px",
+    xl: "1280px",
+
   },
   config: {
     initialColorMode: "light",
+  },
+  colors: {
+    background: {
+      50: "#0184ff",
+      100: "#bf40ff",
+    },
+    text: {
+
+    },
+  },
+  textStyles: {
+    navbar: {
+      fontWeight: 400,
+      fontSize: "24px",
+      lineHeight: "140%",
+      fontFamily: sen.style.fontFamily,
+      color: "white",
+    },
+    homeText1: {
+      fontWeight: 400,
+      fontSize: ["24px", null, "24px", "36px", "44px" ],
+      lineHeight: "160%",
+      fontFamily: sen.style.fontFamily,
+      color: "white",
+    },
+
+    homeText2: {
+      fontWeight: 400,
+      fontSize: "20px",
+      lineHeight: "160%",
+      fontFamily: sen.style.fontFamily,
+      color: "white",
+    },
+
+
+    buttonText1: {
+      fontWeight: 400,
+      fontSize: "30px",
+      lineHeight: "160%",
+      fontFamily: sen.style.fontFamily,
+      color: "white",
+    },
+  },
+
+  components: {
+    Button: {
+      variants: {
+        outlined: {
+          bg: "transparent",
+          color: "white",
+          border: "1px",
+          _hover: { bg: "rgba(0,0,0,0.1)" },
+          _active: { bg: "rgba(0,0,0,0.2)" },
+        },
+      },
+    },
   },
 });
 
